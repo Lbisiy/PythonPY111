2cc30a5850f75220bc1cb511574dae398e9438a4
Marat <115390373+Lbisiy@users.noreply.github.com>
2023-03-22 13:57:20 +0100

    Update task.py

--- Динамическое программирование/Лабораторная 4/Автомобиль. Количество маршрутов/task.py
+++ Динамическое программирование/Лабораторная 4/Автомобиль. Количество маршрутов/task.py
@@ -13,12 +13,6 @@ def car_paths(n: int, m: int) -> List[List[int]]:
 
     table = [[1 for _ in range(m)] for _ in range(n)]
 
-    for row in range(n - 1):
-        table[row + 1][0] = 1
-
-    for col in range(m - 1):
-        table[0][col + 1] = 1
-
     for i in range(1, n):
         for j in range(1, m):
             table[i][j] = table[i - 1][j] + table[i][j - 1] + table[i - 1][j - 1]
